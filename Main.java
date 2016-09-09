62cf60232fbba9259942a3019a82beb0a7a5c3f0
jigs031 <jigs031@gmail.com>
2016-09-09 19:12:54 +0530

    First Commit  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   Main.java
    	new file:   Person.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    int x;
+    public  Main(int x){
+        this.x=x;
+    }
+
+    public static void main(String args[]){
+        Main m=new Main(34);
+    }
+}
---
+++ Person.java
@@ -0,0 +1,10 @@
+public class Person{
+    int x;
+    public  Person(int x){
+        this.x=x;
+    }
+
+    public static void main(String args[]){
+        Person m=new Person(34);
+    }
+}
